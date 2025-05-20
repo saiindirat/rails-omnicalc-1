4c5b212d5ba3b12cabc04fb61182f10ccf4af391
saiindirat <tholusaiindira@gmail.com>
2025-05-20 02:28:11 +0000

    commit554647

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get("/square/new", { :controller => "calculations", :action => "square_form" })
-  get("/square/results", { :controller => "calculations", :action => "square_results" })
-  
-  root({ :controller => "calculations", :action => "square_form" })
+  get("/square/new",       { :controller => "calculations", :action => "square_form" })
+  get("/square/results",   { :controller => "calculations", :action => "square" })
+
+  get("/square_root/new",     { :controller => "calculations", :action => "square_root_form" })
+  get("/square_root/results", { :controller => "calculations", :action => "square_root" })
+
+  get("/payment/new",     { :controller => "calculations", :action => "payment_form" })
+  get("/payment/results", { :controller => "calculations", :action => "payment" })
+
+  get("/random/new",     { :controller => "calculations", :action => "random_form" })
+  get("/random/results", { :controller => "calculations", :action => "random" })
 end
 
